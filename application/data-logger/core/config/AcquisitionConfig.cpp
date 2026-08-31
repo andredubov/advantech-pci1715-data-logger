2d312394e60e5857c922e6d7f89a3ee3a2bf457d
Andrei Dubov <av.dubov@yandex.ru>
2026-08-31 18:14:54 +0400

    chore: update default sampling rate and samples per channel for PCI-1715

--- application/data-logger/core/config/AcquisitionConfig.cpp
+++ application/data-logger/core/config/AcquisitionConfig.cpp
@@ -5,8 +5,8 @@ namespace app {
 AcquisitionConfig::AcquisitionConfig()
     : startChannel(0)
     , channelCount(1)
-    , samplesPerChannel(25000)
-    , samplingRate(250000.0)
+    , samplesPerChannel(10240)
+    , samplingRate(500000.0)
     , inputMode("bipolar")
     , inputRange("10V")
 {
